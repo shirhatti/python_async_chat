7241692657435b951a50d1955f58ab178cd9fe1d
Sourabh Shirhatti <shirhatti@outlook.com>
2014-09-13 23:09:03 -0400

    Initial upload

---
+++ read.py
@@ -0,0 +1,26 @@
+import asynchat
+import asyncore
+import socket
+ 
+class ChatClient(asynchat.async_chat):
+ 
+    def __init__(self, host, port):
+        asynchat.async_chat.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect((host, port))
+ 
+        self.set_terminator(b'\n')
+        self.buffer = []
+ 
+    def collect_incoming_data(self, data):
+        self.buffer.append(data)
+ 
+    def found_terminator(self):
+        msg = b''.join(self.buffer)
+        print 'Received:', msg
+        self.buffer = []
+ 
+client = ChatClient('beta.shirhatti.com', 5051)
+ 
+print 'Listening on localhost:5051'
+asyncore.loop()
---
+++ send.py
@@ -0,0 +1,31 @@
+import asynchat
+import asyncore
+import socket
+import threading
+ 
+
+class ChatClient(asynchat.async_chat):
+ 
+    def __init__(self, host, port):
+        asynchat.async_chat.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect((host, port))
+ 
+        self.set_terminator(b'\n')
+        self.buffer = []
+ 
+    def collect_incoming_data(self, data):
+        pass
+ 
+    def found_terminator(self):
+        pass
+ 
+client = ChatClient('beta.shirhatti.com', 5051)
+ 
+comm = threading.Thread(target=asyncore.loop)
+comm.daemon = True
+comm.start()
+ 
+while True:
+    msg = input('> ')
+    client.push(bytearray(msg + '\n', 'utf-8'))
